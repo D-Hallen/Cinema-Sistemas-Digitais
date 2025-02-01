d6e5574e50231417c0835e491b8ec63d2538434b
Daniel Hallen <117290234+D-Hallen@users.noreply.github.com>
2025-02-01 15:50:39 -0300

    Create CORSConfig.java
    Criação configuração CORS

---
+++ cinema/src/main/java/com/sisdist/cinema/CORSConfig.java
@@ -0,0 +1,18 @@
+package com.sisdist.cinema;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS para todos os endpoints
+                .allowedOrigins("http://localhost:5173") // Permite apenas o domínio do frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Headers permitidos
+                .allowCredentials(true); // Permite credenciais (cookies, headers de autenticação, etc.)
+    }
+}
